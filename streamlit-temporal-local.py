2f760cc53c68065b60f06966d93203cb534e67b3
Deadpool1108 <masonfromct@gmail.com>
2024-08-05 20:46:51 -0400

    Update streamlit-temporal-local.py

--- streamlit-temporal-local.py
+++ streamlit-temporal-local.py
@@ -1,25 +1,14 @@
 import pandas as pd
 import streamlit as st
-#import plotly.express as px
+import matplotlib.pyplot as plt
 
 # Load the Excel data
 df = pd.read_excel('global-monthly-temp-anomaly.xlsx', sheet_name='YourSheetName')
 
-# Convert date column to datetime
-df['Date'] = pd.to_datetime(df['Date'])
-
 # Streamlit app
 st.title('Global Monthly Temperature Anomaly')
 
-# User input for year range
-year_range = st.slider('Select Year Range', min_value=df['Date'].dt.year.min(),
-                       max_value=df['Date'].dt.year.max(), value=[df['Date'].dt.year.min(), df['Date'].dt.year.max()])
-
-# Filter data based on year range
-filtered_df = df[(df['Date'].dt.year >= year_range[0]) & (df['Date'].dt.year <= year_range[1])]
-
-# Create interactive line chart
-#fig = px.line(filtered_df, x='Date', y='Anomaly', title='Global Monthly Temperature Anomaly')
-st.plotly_chart(fig)
+# Line chart
+st.line_chart(df.set_index('Date')['Anomaly'])
 
 
